958638c958e1d053f4780b5bfbdeda8afd0f3e9a
keigotanaka <dezikuta18@yahoo.co.jp>
2013-07-23 00:46:58 +0900

    計算を行うJavascript、HTML

---
+++ js/shopping.js
@@ -0,0 +1,28 @@
+function calculate(){
+	'use strict';
+	
+	var total;
+	var quantity = document.getElementById('quantity').value;
+	var price = document.getElementById('price').value;
+	var tax = document.getElementById('tax').value;
+	var discount = document.getElementById('discount').value;
+	
+	total = quantity * price;
+	
+	tax /= 100;
+	tax++;
+	total *= tax;
+    total -= discount;
+	
+	total = total.toFixed(2);
+	
+	document.getElementById('total').value = total;
+	return false;
+}
+
+function init(){
+	'use strict'
+	var theForm = document.getElementById('theForm');
+	theForm.onsubmit = calculate;
+}
+window.onload = init;
